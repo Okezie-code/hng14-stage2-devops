f80f6ac35843d9a1623a61cb88c59565cc75d463
Victor Okezie <hngdevops@ip-172-31-29-221.eu-north-1.compute.internal>
2026-04-21 07:48:22 +0000

    Dockerized app with API, worker,  frontend and Redis

--- worker/worker.py
+++ worker/worker.py
@@ -3,13 +3,27 @@ import time
 import os
 import signal
 
-r = redis.Redis(host="localhost", port=6379)
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379))
+)
 
 def process_job(job_id):
     print(f"Processing job {job_id}")
     time.sleep(2)  # simulate work
     r.hset(f"job:{job_id}", "status", "completed")
     print(f"Done: {job_id}")
+
+
+while True:
+    try:
+        r.ping()
+        print("Connected to Redis")
+        break
+    except redis.exceptions.ConnectionError:
+        print("Waiting for Redis...")
+        time.sleep(2)
+
 
 while True:
     job = r.brpop("job", timeout=5)
